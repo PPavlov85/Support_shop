a1185a531311c03b952afdbf85e7ca3cdcb3d0f4
Pavel Pavlov <gsxr_1000@abv.bg>
2021-06-06 19:33:53 +0300

    Create product details view

--- product/views.py
+++ product/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+
 from django.shortcuts import render
 
 from rest_framework.views import APIView
@@ -12,3 +14,17 @@ class LatestProductList(APIView):
         products = Product.objects.all()[0:4]
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
+
+class ProductDetails(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
