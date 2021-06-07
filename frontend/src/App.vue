d3c804835f79b0fff266010b7db6926cbb66736e
Pavel Pavlov <gsxr_1000@abv.bg>
2021-06-07 20:10:20 +0300

    frontend application
    Install and set Vue, create front page, visualisation products, details, create add button

---
+++ frontend/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+    <div id="wrapper">
+        <nav class="navbar is-dark">
+            <div class="navbar-brand">
+                <router-link to="/" class="navbar-item"><strong>Moto-eShop</strong></router-link>
+
+                <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu"
+                   @click="showMobileMenu = !showMobileMenu">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
+                <div class="navbar-end">
+
+                    <router-link to="/parts" class="navbar-item">Parts</router-link>
+                    <router-link to="/clothes" class="navbar-item">Clothes</router-link>
+
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <router-link to="/log-in" class="button is-success">Log in</router-link>
+
+                            <router-link to="/cart" class="button is-success">
+                                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                                <span>Cart ({{ cartTotalLength }})</span>
+                            </router-link>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </nav>
+
+        <section class="section">
+            <router-view/>
+        </section>
+
+        <footer class="footer">
+            <p class="has-text-centered">Copyright (c) 2021</p>
+        </footer>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            showMobileMenu: false,
+            cart: {
+                items: []
+            }
+        }
+    },
+    beforeCreate() {
+        this.$store.commit('initializeStore')
+    },
+    mounted() {
+        this.cart = this.$store.state.cart
+    },
+    computed: {
+        cartTotalLength() {
+            let totalLength = 0
+
+            for (let i = 0; i < this.cart.items.length; i++) {
+                totalLength += this.cart.items[i].quantity
+            }
+
+            return totalLength
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import "../node_modules/bulma";
+</style>
