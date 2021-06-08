061e8761e251d9fde6f903ec5eb2f00f2cba9235
Pavel Pavlov <gsxr_1000@abv.bg>
2021-06-08 23:27:36 +0300

    create add to cart message

--- frontend/src/views/Product.vue
+++ frontend/src/views/Product.vue
@@ -31,6 +31,7 @@
 
 <script>
 import axios from 'axios'
+import {toast} from 'bulma-toast'
 
 export default {
     name: "Product",
@@ -68,6 +69,15 @@ export default {
             }
 
             this.$store.commit('addToCart', item)
+
+            toast({
+                message: 'The product  was added to the cart',
+                type: 'is-success',
+                dismissible: true,
+                pauseOnHover: true,
+                duration: 2000,
+                position: 'bottom-right'
+            })
         }
     }
 }
